f946bd1bd9a311f40ae729b577fc4edea6c5e059
robert Salazar <11rsahome@gmail.com>
2019-12-08 14:57:27 +0100

    create pruebas

---
+++ index.js
@@ -0,0 +1,11 @@
+module.export = {
+    hola: function() {
+        console.log("Hola mundo");
+    },
+    holaPersonalizado: function(nombre) {
+        console.log(`hola ${nombre}`)
+    },
+    holaAlCursoNPM: function() {
+        console.log("Hola al curson npm");
+    }
+}
